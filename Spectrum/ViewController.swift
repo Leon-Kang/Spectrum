c4bb3f1010952228b1b53433f0ebc3f6ac457f63
Leon-Kang <coderleonkang@gmail.com>
2019-07-17 16:00:37 +0800

    Initial Commit

---
+++ Spectrum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spectrum
+//
+//  Created by Leon Kang on 2019/7/17.
+//  Copyright © 2019 Leon Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
